8949ffeba43231436cfc0ffec6768862a705e000
saldulander <tomysaldutto@hotmail.com>
2016-07-09 18:31:24 -0300

    files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160707052152) do
+ActiveRecord::Schema.define(version: 20160707054207) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
